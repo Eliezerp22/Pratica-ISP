e6782f693fcccf7abff9ea544f96e33cbd36b17d
Eliezer Garcia <Eliezerp.garcia@hotmail.com>
2024-10-30 20:14:24 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_ISP
+{
+    public interface IAve
+    {
+        void Volar();
+        void Cantar();
+        void PonerHuevos();
+    }
+
+    public interface IAveNadadora
+    {
+        void Nadar();
+    }
+
+    public interface IAveCazadora
+    {
+        void Cazar();
+    }
+
+
+}
